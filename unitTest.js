2090bc8f846b634d17e1a58551abdd6314e07ea6
nnouhi <nnouhi@uclan.ac.uk>
2021-03-04 17:23:20 +0200

    Added the container for the question's unit test and their function, tomorrow i will finish with the whole unit test page as well as the questions test

--- unitTest.js
+++ unitTest.js
@@ -1,6 +1,10 @@
 let hasPrize="&no";
 let sorted="&no";
 let completed="&no";
+let completedQ="&no";
+let geolocation="&no";
+let skipped="&no";
+
 
 /* Function for the sorted check box */
 function checkBoxSorted(){
@@ -44,11 +48,57 @@ function checkBoxCompleted(){
     }
 }
 
+/* Function for the completed Question check box */
+function checkBoxCompletedQ(){
+
+    let completedQCheckBox=document.getElementById("completedQCheckBox");
+    let completedQP=document.getElementById("completedQP");
+    if(completedQCheckBox.checked === true){
+        completedQ="&completed";
+        completedQP.style.display="block";
+    }
+    else{
+        completedQ="&no";
+        completedQP.style.display="none";
+    }
+}
+
+/* Function for the geolocation check box */
+function checkBoxGeolocation(){
+
+    let geolocationCheckBox=document.getElementById("GeolocationCheckBox");
+    let geolocationQP=document.getElementById("geolocationP");
+    if(geolocationCheckBox.checked === true){
+        geolocation ="&requires-location";
+        geolocationQP.style.display="block";
+    }
+    else{
+        geolocation="&no";
+        geolocationQP.style.display="none";
+    }
+}
+
+/* Function for the skip check box */
+function checkBoxSkip(){
+
+    let skipCheckBox=document.getElementById("SkipCheckBox");
+    let skipP=document.getElementById("skipP");
+    if(skipCheckBox.checked === true){
+        skipped ="&can-be-skipped";
+        skipP.style.display="block";
+    }
+    else{
+        skipped="&no";
+        skipP.style.display="none";
+    }
+}
+
 /* Function that tests the leaderboards*/
 function getNumPlayers(){
     let numberOfPlayers=document.getElementById("numberOfPlayers").value;
     setTimeout(function(){ window.location.href="leaderboards.html?testLB"+sorted+hasPrize+"&size="+numberOfPlayers;},1000)
 }
+
 
 /* Function that tests the player score*/
 function getScore(){
