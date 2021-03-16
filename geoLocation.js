b0529729e138ec68d7ab3fe73c7d0399dd60c17c
nnouhi <nnouhi@uclan.ac.uk>
2021-03-16 19:00:55 +0200

    Fixed the the completion time in Leaderboards (unit timestamp to seconds), added a countdown for periodical location updates

--- geoLocation.js
+++ geoLocation.js
@@ -1,10 +1,7 @@
-/*geoLocation.js script
-* Description: Includes functions for periodic
-*  update and force location update*/
-
 /*Global variables*/
 let lat;
 let lng;
+let countdown=60;
 
 /*Functions used for periodically updating the location*/
 /*Periodically update the lat and lng  every 5 seconds*/
@@ -16,7 +13,8 @@ function updateLocation_Period() {
         alert("Geolocation is not supported by your browser.");
     }
 
-    setTimeout(updateLocation_Period,5000); /*-> maybe will have to update every 25 seconds*/
+    setTimeout(updateLocation_Period,5000);
+
 }
 /*Updates global variables lat, long*/
 function updateLatLong(position){
@@ -27,11 +25,12 @@ function updateLatLong(position){
 
 /*Updates user location periodically every 30 secs*/
 function updateUsersLocation(){
-    console.log("Update users location, 30 secs");
+    console.log("Update users location");
     fetch(TH_BASE_URL_LOCATION + getCookie("sessionID") + "&latitude=" + lat + "&longitude=" + lng)
         .then(response => response.json())
         .then(jsonObject => {});
-    setTimeout(updateUsersLocation,30000);
+    setTimeout(updateUsersLocation,60000);
+    setTimeout(timer(countdown),60000);
 }
 /*Functions used for periodically updating the location*/
 
@@ -39,7 +38,7 @@ function updateUsersLocation(){
 
 /*Force update users location*/
 function updateLocation_Force() {
-    console.log("force location update test 1");
+    console.log("force location update");
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(updateUsersLocation_Force);
     } else {
@@ -48,7 +47,7 @@ function updateLocation_Force() {
 }
 /*Force update users location with the current lng and lat*/
 function updateUsersLocation_Force(position){
-    console.log("force location update test 2");
+    console.log("force location update2");
     lat=position.coords.latitude;
     lng=position.coords.longitude;
     fetch(TH_BASE_URL_LOCATION + getCookie("sessionID") + "&latitude=" + lat + "&longitude=" + lng)
@@ -56,3 +55,15 @@ function updateUsersLocation_Force(position){
         .then(jsonObject => {});
 }
 /*Functions used for force updating the Location*/
+
+let timeElm = document.getElementById("periodicLoc");
+
+let timer = function(countdown) {
+
+    if(countdown === 0) {
+        return;
+    }
+    timeElm.innerHTML = "Next location update in "+"<span style='color:darksalmon'>"+countdown+"</span>"+" seconds.";
+
+    return setTimeout(() => {timer(--countdown)}, 1000)
+}
